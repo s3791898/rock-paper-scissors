f57b9ae28644b133c90e60d47290b296cb6e75bd
Anthony Tsui <68526943+s3791898@users.noreply.github.com>
2023-12-08 23:36:46 +1100

    Added UI functionality
    Turned the console based rps game into a UI based rps game

--- script.js
+++ script.js
@@ -1,84 +1,112 @@
-function capitaliseFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 3);
-
-  switch (computerChoice) {
-    case 0:
-      return "Rock";
-    case 1:
-      return "Paper";
-    case 2:
-      return "Scissor";
-    default:
-      return "Invalid choice";
-  }
-}
-
-function playRound(playerSelection, computerSelection) {
-  const playerChoice = playerSelection.toLowerCase();
-  const computerChoice = computerSelection.toLowerCase();
-
-  if (playerChoice === computerChoice) {
-    return "Tie";
-  } else if (playerChoice === "rock" && computerChoice === "scissor") {
-    return "You Win! Rock beats Scissor";
-  } else if (playerChoice === "paper" && computerChoice === "rock") {
-    return "You Win! Paper beats Rock";
-  } else if (playerChoice === "scissor" && computerChoice === "paper") {
-    return "You Win! Scissor beats Paper";
-  } else {
-    return `You Lose! ${capitaliseFirstLetter(
-      computerChoice
-    )} beats ${capitaliseFirstLetter(playerChoice)}`;
-  }
-}
-
 function game() {
   let playerScore = 0;
   let computerScore = 0;
-  const validChoices = ["rock", "paper", "scissor"];
+  let moves = 0;
 
-  for (let round = 1; round <= 5; round++) {
-    let playerSelection = prompt(
-      "Enter your choice (rock, paper, or scissor):"
-    ).toLowerCase();
+  // Function to start playing the game
+  function playGame() {
+    const rockBtn = document.querySelector(".rock");
+    const paperBtn = document.querySelector(".paper");
+    const scissorBtn = document.querySelector(".scissor");
+    const playerOptions = [rockBtn, paperBtn, scissorBtn];
+    const computerOptions = ["rock", "paper", "scissors"];
 
-    // Validate user input
-    while (!validChoices.includes(playerSelection)) {
-      alert("Invalid choice! Please enter rock, paper or scissor.");
-      playerSelection = prompt(
-        "Enter your choice (rock, paper, or scissor):"
-      ).toLowerCase();
+    playerOptions.forEach((option) => {
+      option.addEventListener("click", function () {
+        const movesLeft = document.querySelector(".movesLeft");
+        moves++;
+        movesLeft.innerText = `Moves Left: ${10 - moves}`;
+
+        const computerChoiceNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerChoiceNumber];
+
+        // Function to check who wins
+        winner(this.innerText, computerChoice);
+
+        if (moves == 10) {
+          gameOver(playerOptions, movesLeft);
+        }
+      });
+    });
+  }
+
+  // Function to decide the winner
+  function winner(player, computer) {
+    const result = document.querySelector(".result");
+    const playerScoreCount = document.querySelector(".p-count");
+    const computerScoreCount = document.querySelector(".c-count");
+    player = player.toLowerCase();
+    computer = computer.toLowerCase();
+    if (player === computer) {
+      result.textContent = "Draw";
+    } else if (player == "rock") {
+      if (computer == "paper") {
+        result.textContent = "Computer Won";
+        computerScore++;
+        computerScoreCount.textContent = computerScore;
+      } else {
+        result.textContent = "Player Won";
+        playerScore++;
+        playerScoreCount.textContent = playerScore;
+      }
+    } else if (player == "paper") {
+      if (computer == "scissors") {
+        result.textContent = "Computer Won";
+        computerScore++;
+        computerScoreCount.textContent = computerScore;
+      } else {
+        result.textContent = "Player Won";
+        playerScore++;
+        playerScoreCount.textContent = playerScore;
+      }
+    } else if (player == "scissors") {
+      if (computer == "rock") {
+        result.textContent = "Computer Won";
+        computerScore++;
+        computerScoreCount.textContent = computerScore;
+      } else {
+        result.textContent = "Player Won";
+        playerScore++;
+        playerScoreCount.textContent = playerScore;
+      }
     }
+  }
 
-    const computerSelection = getComputerChoice();
-    const result = playRound(playerSelection, computerSelection);
+  // Function to run when the game is over
+  function gameOver(playerOptions, movesLeft) {
+    const chooseMove = document.querySelector(".move");
+    const result = document.querySelector(".result");
+    const reloadBtn = document.querySelector(".reload");
 
-    if (result.includes("You Win!")) {
-      playerScore++;
-      alert("You Won this round!");
-    } else if (result.includes("You Lose!")) {
-      computerScore++;
-      alert("You Lost this round...");
+    playerOptions.forEach((option) => {
+      option.style.display = "none";
+    });
+
+    chooseMove.innerText = "Game Over";
+    chooseMove.style.fontSize = "24px";
+    movesLeft.style.display = "none";
+
+    if (playerScore > computerScore) {
+      result.innerText = "Congratulations You Won!";
+      result.style.color = "#308D46";
+      result.style.fontSize = "24px";
+    } else if (playerScore < computerScore) {
+      result.innerText = "Unfortunately You Lost...";
+      result.style.color = "red";
+      result.style.fontSize = "24px";
     } else {
-      alert("This round was a Tie!");
+      result.innerText = "Draw";
+      result.style.color = "grey";
+      result.style.fontSize = "24px";
     }
+    reloadBtn.innerText = "Restart";
+    reloadBtn.style.display = "flex";
+    reloadBtn.addEventListener("click", function () {
+      window.location.reload();
+    });
   }
 
-  console.log("Game Over!");
-  console.log(`Player's Score: ${playerScore}`);
-  console.log(`Computer's Score: ${computerScore}`);
-
-  if (playerScore > computerScore) {
-    console.log("Congratulations You Won!");
-  } else if (playerScore < computerScore) {
-    console.log("You Lost, Better Luck Next Time!");
-  } else {
-    console.log("It's a Tie!");
-  }
+  playGame();
 }
 
 game();
